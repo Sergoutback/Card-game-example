0f34119b10248f2a883cc07d0288464dd68dad6d
Sergoutback <webart2013@gmail.com>
2024-04-18 18:36:28 +0300

    Add Scene Switcher. Add GameScene left column

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    //for the future
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+    public void NextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
